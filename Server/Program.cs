267aa45e3ea2e9e77d064584a6cb5058dd2a7e6f
alexa <alexa@DESKTOP-JH6OCVP>
2017-05-24 14:16:52 +0100

    Criação de keys

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,23 @@ namespace Server
 {
     class Program
     {
+        private static RSACryptoServiceProvider rsa;
+
         static void Main(string[] args)
         {
-            //conectar cliente (Guardar chaves
+            rsa = new RSACryptoServiceProvider();
+            //Criação de chaves privada/publica.
+            string publicKey = rsa.ToXmlString(false);
+            string privateKey = rsa.ToXmlString(true);
+
+            //Guardar as chaves em ficheiros.
+            File.WriteAllText("publicKey.txt", publicKey);
+            File.WriteAllText("publicPrivateKey.txt", privateKey);
+
+            Console.WriteLine(publicKey);
+            Console.WriteLine("");
+            Console.WriteLine(privateKey);
+            Console.ReadKey();
         }
     }
 }
