78abda97b8be5307496a72f2a8ab401f8e9efa1d
Goku-san <contas.jogos.servicos@gmail.com>
2017-05-24 14:51:38 +0100

    coppied register

--- Server/Program.cs
+++ Server/Program.cs
@@ -27,7 +27,7 @@ namespace Server
             
         }
 
-        private bool VerifyLogin()
+        private bool VerifyLogin(string username, string password)
         {
             //Conecção à base de dados.
             SqlConnection conn = null;
@@ -45,6 +45,28 @@ namespace Server
                 SqlCommand cmd = new SqlCommand();
                 cmd.CommandText = sql;
 
+                // Declaração dos parâmetros do comando SQL
+                SqlParameter param = new SqlParameter("@username", username);
+
+                // Introduzir valor ao parâmentro registado no comando SQL
+                cmd.Parameters.Add(param);
+
+                // Associar ligação à Base de Dados ao comando a ser executado
+                cmd.Connection = conn;
+
+                // Executar comando SQL
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (!reader.HasRows)
+                {
+                    throw new Exception("Error while trying to access an user");
+                }
+
+                // Ler resultado da pesquisa
+                reader.Read();
+
+
+
                 return true;
 
 
@@ -56,5 +78,51 @@ namespace Server
                 return false;
             }
         }
+
+        private void Register(string username, byte[] saltedPasswordHash, byte[] salt)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                // Configurar ligação à Base de Dados
+                conn = new SqlConnection();
+                conn.ConnectionString = String.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Users\DIOGO\Documents\Visual Studio 2015\Projects\TopicosSegurancaFicha8Solution\FichaPratica8_Base\Database.mdf';Integrated Security=True");
+
+                // Abrir ligação à Base de Dados
+                conn.Open();
+
+                // Declaração dos parâmetros do comando SQL
+                SqlParameter paramUsername = new SqlParameter("@username", username);
+                SqlParameter paramPassHash = new SqlParameter("@saltedPasswordHash", saltedPasswordHash);
+                SqlParameter paramSalt = new SqlParameter("@salt", salt);
+
+                // Declaração do comando SQL
+                String sql = "INSERT INTO Users (Username, SaltedPasswordHash, Salt) VALUES (@username,@saltedPasswordHash,@salt)";
+
+                // Prepara comando SQL para ser executado na Base de Dados
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                // Introduzir valores aos parâmentros registados no comando SQL
+                cmd.Parameters.Add(paramUsername);
+                cmd.Parameters.Add(paramPassHash);
+                cmd.Parameters.Add(paramSalt);
+
+                // Executar comando SQL
+                int lines = cmd.ExecuteNonQuery();
+
+                // Fechar ligação
+                conn.Close();
+                if (lines == 0)
+                {
+                    // Se forem devolvidas 0 linhas alteradas então o não foi executado com sucesso
+                    throw new Exception("Error while inserting an user");
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error while inserting an user:" + e.Message);
+            }
+        }
+
     }
 }
